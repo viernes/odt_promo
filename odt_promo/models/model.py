31b5c7c68ccc46dc374f538cc68896ad5a8a199b
Unknown <williamcolin527@gmail.com>
2018-11-04 12:11:30 -0600

    .

--- odt_promo/models/model.py
+++ odt_promo/models/model.py
@@ -16,6 +16,35 @@ class crmlead(models.Model):
 	slogan_marca = fields.Char(string='Eslogan')
 	logo_marca = fields.Binary(string='Logo')
 
+	""" Quiz Fields """
+
+	rp_1 = fields.Char(string='¿Para que estamos haciendo este proyecto y cual es el reto?')
+	rp_2 = fields.Char(string='¿Que queremos que se sepa y sienta la gente sobre el proeycto?')
+	rp_3 = fields.Char(string='¿Que buscamos lograr?')
+	rp_4 = fields.Char(string='¿Cual es el problema a resolver?')
+	on_1 = fields.Char(string='¿Crecimiento?')
+	on_2 = fields.Char(string='¿Mayor margen de utilidad?')
+	on_3 = fields.Char(string='¿Posicionamiento de un nuevo producto o servicio?')
+	on_4 = fields.Char(string='¿Hacer frente a la competencia?')
+	ob_1 = fields.Char(string='¿Conocimiento?')
+	ob_2 = fields.Char(string='¿Posicionamiento?')
+	ob_3 = fields.Char(string='¿Diferenciacion?')
+	cm_1 = fields.Char(string='¿Como se define y posiciona la marca en cuanto a si misma?')
+	cm_2 = fields.Char(string='¿Que linea de comunicación esta implementando la marca actualmente?')
+	cm_3 = fields.Char(string='¿Descripcion de la marca (Joven, solida, dinamica, innovadora, flexible, segura, institucional, preocupada por el consumidor)?')
+	cm_4 = fields.Char(string='¿Que tono se debe adoptar?')
+	5q_1 = fields.Char(string='¿Quiénes son loc competidores?')
+	5q_2 = fields.Char(string='¿En qué se diferencia la marca ante la competencia (beneficios al consumidor)?')
+	5q_3 = fields.Char(string='¿Qué piensan y sienten los consumidores acerca de la competencia?')
+	6vh_1 = fields.Char(string='¿NSE, TARGET?')
+	6vh_2 = fields.Char(string='¿Cuál es el comportamiengo habitual?')
+	6vh_3 = fields.Char(string='¿Qué piensan y sienten acerca de la marca?')
+	7dc_1 = fields.Char(string='7. ¿QUÉ DEBEMOS COMUNICAR?')
+	7dc_2 = fields.Char(string='¿Qué queremos que piensen y sientan de la marca?')
+	7dc_3 = fields.Char(string='¿Qué queremos que se sepa y sienta la gente sobre esta comunicación?')
+	8qc_1 = fields.Char(string='8. ¿QUÉ NO QUEREMOS COMUNICAR?')
+	8qc_2 = fields.Char(string='¿CÓMO SE COMPORTA EL CONSUMIDOR RESPECTO AL PRODUCTO O SERVICIO ACTUALMENTE (CONDUCTAS Y CARENCIAS)?')
+
 class marca_crm(models.Model):
 	"""docstring for marca_crm"""
 	_name = 'crm_marca'
